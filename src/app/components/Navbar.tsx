fbbc4da4f1e3dfa53cbb195ffb10a5569fc10cd8
Anjum109 <aunjumtuli109@gmail.com>
2025-08-09 19:23:41 +0600

    correction one

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -14,7 +14,8 @@ export default function Navbar() {
 
     return (
         <nav className="bg-[#04070c] shadow p-4 flex justify-between items-center border-b-2 border-blue-500 py-5 px-5 lg:px-20">
-            <h1 className="text-xl font-bold text-blue-200">🧠 iFarmer Assignment</h1>
+            <h1 className="text-xl font-bold text-blue-200 lg:hidden">🧠 iFarmer </h1>
+            <h1 className="text-xl font-bold text-blue-200 hidden lg:block">🧠 iFarmer Assignment </h1>
             <div className="flex flex-col lg:flex-row  gap-5">
                 <div className='text-[12px] lg:text-[22px]'>
                     <Link href="/assignment_1" className={linkClasses('/assignment_1')}>
